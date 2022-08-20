17e168c6f367bfa838fd456e378889f1391628ce
KaynatSiddiqui <107025229+KaynatSiddiqui@users.noreply.github.com>
2022-08-20 13:10:57 +0530

    atmprocess

---
+++ src/kay/git/com/Atm.java
@@ -0,0 +1,27 @@
+package kay.git.com;
+
+public class Atm {
+	public static void main(String args[])
+	{
+		float amt=5000;
+		String userPassword="123";
+		float withdrawAmt=4000;
+		
+		if(userPassword=="123")
+		{
+		if(withdrawAmt>amt)
+		{
+			System.out.println("insufficient balance");
+		}
+		else
+		{
+			float rem=amt-withdrawAmt;
+			System.out.println("your balance is:"+rem);
+		}
+		}else
+		{
+			System.out.println("wrong password");
+		}
+	}
+
+}
