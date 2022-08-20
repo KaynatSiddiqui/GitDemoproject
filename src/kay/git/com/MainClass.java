81271e1038fc115cb6c87d5bcedd516c87dddabb
KaynatSiddiqui <107025229+KaynatSiddiqui@users.noreply.github.com>
2022-08-20 12:19:46 +0530

    First java project to commit

---
+++ src/kay/git/com/MainClass.java
@@ -0,0 +1,19 @@
+package kay.git.com;
+
+public class MainClass {
+
+	public static void main(String args[])
+	{
+		System.out.println("kaynat are you hungry?");
+		String hungry="yes";
+		if(hungry=="yes")
+		{
+			System.out.println("Eat something please");
+		}
+		else
+		{
+			System.out.println("Chill bro");
+		}
+	}
+	
+}
