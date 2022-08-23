7ef349c8fd2ba0aa654ab2b91decdbe7cf34f308
KaynatSiddiqui <107025229+KaynatSiddiqui@users.noreply.github.com>
2022-08-23 13:28:30 +0530

    Right Triangle Pattern

---
+++ src/kay/git/com/RightTrianglePattern.java
@@ -0,0 +1,17 @@
+package kay.git.com;
+
+public class RightTrianglePattern {
+	public static void main(String[] args)
+	{
+		int number=5;
+		for(int i=0;i<number;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+	}
+
+}
